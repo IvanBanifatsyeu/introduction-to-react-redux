d020d758075005cea75eba37192dd5c7c9eade37
IvanBanifatsyeu <gavrik2010@yahoo.com>
2024-04-23 23:04:10 +0300

    the simplest example of REDUX

--- src/index.js
+++ src/index.js
@@ -1,7 +1,78 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider, useSelector, useDispatch } from "react-redux";
 import "./index.css";
-import App from "./App";
+import { legacy_createStore as createStore } from "redux";
+import { useState } from "react";
+
+const initialState = {
+	name: "Jack",
+};
+
+const reducer = (state = initialState, action) => {
+	switch (action.type) {
+		case "UPDATE_NAME": {
+			return {
+				...state,
+				...action.payload,
+			};
+		}
+		default:
+			return state;
+	}
+};
+
+const setName = (obj) => {
+	return {
+		type: "UPDATE_NAME",
+		payload: obj,
+	};
+};
+
+const store = createStore(
+	reducer,
+	window.__REDUX_DEVTOOLS_EXTENSION__
+		? window.__REDUX_DEVTOOLS_EXTENSION__()
+		: () => {}
+);
+
+const App = () => {
+	const [inputName, setInputName] = useState("");
+	const dispatch = useDispatch();
+
+	const changeNAme = () => {
+		if (inputName.length > 0) {
+			dispatch(setName({ name: inputName }));
+		}
+
+		setInputName("");
+	};
+
+	const handleInput = (e) => {
+		setInputName(e.target.value);
+	};
+
+	return (
+		<>
+			<input type="text" value={inputName} onChange={handleInput} />
+			<button onClick={changeNAme}> Change Name</button>;
+		</>
+	);
+};
+
+const ViewName = () => {
+	const storeName = useSelector((st) => st.name);
+
+	return (
+		<>
+			<h1>{storeName}</h1>
+		</>
+	);
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+	<Provider store={store}>
+		<App />
+		<ViewName />
+	</Provider>
+);
